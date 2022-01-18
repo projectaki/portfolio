640c53f468e6d0ac5bce81a58ba3da0435ca10f3
Akos <65110142+projectaki@users.noreply.github.com>
2022-01-18 19:34:43 +0100

    🪛Configure cypress + 1 passing test (#10)

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
+// For more info, visit https://on.cypress.io/plugins-api
+export default (_on: any, _config: any) => {};
