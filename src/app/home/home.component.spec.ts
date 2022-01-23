9d6624a2dd1153cc058ea6a57427d63d067d1c89
Akos <akosegypro@gmail.com>
2022-01-23 15:23:01 +0100

    :wrench: Fix home comp tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,12 +2,11 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router';
 
-describe('Test home component', () => {
+describe('Home component', () => {
   let fixture;
   let comp: HomeComponent;
-  let router: Router;
+  //let router: Router;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
@@ -16,42 +15,16 @@ describe('Test home component', () => {
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
     fixture = TestBed.createComponent(HomeComponent);
-    router = TestBed.inject(Router);
+    //router = TestBed.inject(Router);
     comp = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  test('Is component initialized', () => {
+  it('should be initialized', () => {
     expect(comp).toBeDefined();
   });
 
-  test('Test input variables', () => {
-    comp.items = [
-      {
-        imgUrl: 'img',
-        func: () => 1,
-      },
-    ];
-    comp.logoUrl = 'logoUrl';
-    expect(comp.items).toBeDefined();
-    expect(comp.items.length).toBe(1);
-    expect(comp.logoUrl).toMatch('logoUrl');
-  });
-
-  test('Test component ngOnInit', () => {
-    const navigateSpy = jest.spyOn(router, 'navigate');
-    comp.ngOnInit();
-    expect(comp.items).toBeDefined();
-    expect(comp.items.length).toBe(5);
-
-    for (let item of comp.items) {
-      const mock = jest.spyOn(item, 'func');
-      expect(item).toBeDefined();
-      const call = item.func;
-      call();
-      expect(mock).toHaveBeenCalledTimes(1);
-      expect(item.imgUrl.length).toBeGreaterThan(0);
-    }
-    expect(navigateSpy).toHaveBeenCalledTimes(5);
+  it('should have defined logoUrl', () => {
+    expect(comp.logoUrl).toBe('assets/logo.png');
   });
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
-import { SpeedDialItem } from '../shared/speed-dial/speeddial-item';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  items!: SpeedDialItem[];
   logoUrl = 'assets/logo.png';
 }
