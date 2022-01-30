ad06ce1d71b39bc185d29a5cc25fe0f4aa4f9622
Akos <akosegypro@gmail.com>
2022-01-30 22:15:34 +0100

    :heavy_plus_sign: Add disable angular plugin

--- scully.portfolio.config.ts
+++ scully.portfolio.config.ts
@@ -1,10 +1,14 @@
 import { ScullyConfig } from '@scullyio/scully';
+const { DisableAngular } = require('scully-plugin-disable-angular');
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer';
 
+const postRenderers = [DisableAngular];
+
 export const config: ScullyConfig = {
   projectRoot: './src',
+  defaultPostRenderers: postRenderers,
   projectName: 'portfolio',
   // add spsModulePath when using de Scully Platform Server,
   outDir: './dist/static',
